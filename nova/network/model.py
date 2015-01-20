f77f75b08b51081913268c1888d5007811ac9cf4
Neil Jerram <Neil.Jerram@metaswitch.com>
2015-01-20 16:46:54 +0000

    Change from VIF_TYPE_ROUTED to VIF_TYPE_TAP
    Change-Id: Id2836e3291b5ef6bc30bc263024472bab74fc07c

--- nova/network/model.py
+++ nova/network/model.py
@@ -38,7 +38,7 @@ VIF_TYPE_802_QBH = '802.1qbh'
 VIF_TYPE_MLNX_DIRECT = 'mlnx_direct'
 VIF_TYPE_MIDONET = 'midonet'
 VIF_TYPE_OTHER = 'other'
-VIF_TYPE_ROUTED = 'routed'
+VIF_TYPE_TAP = 'tap'
 
 # Constants for dictionary keys in the 'vif_details' field in the VIF
 # class
