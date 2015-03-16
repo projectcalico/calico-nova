de749afa55408303e515baa9890551476af2ef5b
Neil Jerram <Neil.Jerram@metaswitch.com>
2015-03-16 12:30:44 +0000

    Apply existing patches
    Change-Id: I177233467611b54482da1b81c393b05826dab9f3

--- .pc/libvirt-Handle-unsupported-host-capabilities.patch/nova/virt/libvirt/driver.py
+++ .pc/libvirt-Handle-unsupported-host-capabilities.patch/nova/virt/libvirt/driver.py
@@ -76,7 +76,6 @@ from nova.objects import service as service_obj
 from nova.openstack.common import excutils
 from nova.openstack.common import fileutils
 from nova.openstack.common.gettextutils import _
-from nova.openstack.common.gettextutils import _LW
 from nova.openstack.common import importutils
 from nova.openstack.common import jsonutils
 from nova.openstack.common import log as logging
@@ -2863,14 +2862,9 @@ class LibvirtDriver(driver.ComputeDriver):
                     #                 this -1 checking should be removed later.
                     if features and features != -1:
                         self._caps.host.cpu.parse_str(features)
-                except libvirt.libvirtError as ex:
-                    error_code = ex.get_error_code()
-                    if error_code == libvirt.VIR_ERR_NO_SUPPORT:
-                        LOG.warn(_LW("URI %(uri)s does not support full set"
-                                     " of host capabilities: " "%(error)s"),
-                                     {'uri': self.uri(), 'error': ex})
-                    else:
-                        raise
+                except libvirt.VIR_ERR_NO_SUPPORT:
+                    # Note(yjiang5): ignore if libvirt has no support
+                    pass
         return self._caps
 
     def get_host_uuid(self):
