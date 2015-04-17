52b87a71d5a4c097fd207035220e7e6aadc20bd2
Emma Gordon <emma@projectcalico.org>
2015-04-17 13:12:43 +0100

    nova_2014.2.2.orig.tar.gz from http://mirror.fuel-infra.org/mos/ubuntu/pool/main/n/nova/

--- nova/network/model.py
+++ nova/network/model.py
@@ -40,7 +40,6 @@ VIF_TYPE_HW_VEB = 'hw_veb'
 VIF_TYPE_MLNX_DIRECT = 'mlnx_direct'
 VIF_TYPE_MIDONET = 'midonet'
 VIF_TYPE_OTHER = 'other'
-VIF_TYPE_TAP = 'tap'
 
 # Constants for dictionary keys in the 'vif_details' field in the VIF
 # class
