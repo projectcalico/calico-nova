73aa67fcbccbe17844cbc8c67e9602816f77efa8
Cory Benfield <cory.benfield@metaswitch.com>
2015-05-22 15:28:32 +0100

    Unapply patches

--- nova/tests/unit/virt/disk/vfs/test_localfs.py
+++ nova/tests/unit/virt/disk/vfs/test_localfs.py
@@ -416,8 +416,7 @@ class VirtDiskVFSLocalFSTest(test.NoDBTestCase):
         execute.assert_called_once_with('blkid', '-o',
                                         'value', '-s',
                                         'TYPE', '/dev/xyz',
-                                        run_as_root=True,
-                                        check_exit_code=[0, 2])
+                                        run_as_root=True)
 
     @mock.patch.object(tempfile, 'mkdtemp')
     @mock.patch.object(nova.virt.disk.mount.nbd, 'NbdMount')
